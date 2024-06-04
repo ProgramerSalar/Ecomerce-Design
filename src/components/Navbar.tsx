161cad323797aba363875d86c0ef39f3e338870e
manish <manishkumar60708090@gmail.com>
2024-06-04 18:15:58 +0530

    header part of html and css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,25 @@
 
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="container">
+            <div className="navbar">
+                <div className="logo">
+                    <img src="src\assets\RedStore_Img\images\logo.png" alt="logo" width="125px" />
+                </div>
+                <nav>
+                    <ul>  {/*  underlist tag */}
+                        <li><a href="">Home</a></li>  {/*  list tag */}
+                        <li><a href="">Product</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Account</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    )
 }
 
 export default Navbar
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
+import { FaArrowRightLong } from "react-icons/fa6"
+import Navbar from "../components/Navbar"
 
 
 const Home = () => {
-  return (
-    <div className="home-container">
-        <div className="nav-container">
-            
+    return (
+        <div className="home-container">
+            <div className="nav-container">
+                <Navbar />
+            </div>
+            <div className="row">
+                <div className="col-2">
+                    <h1>Give Your Workout <br /> A New Style!</h1>
+                    <p>Success isn't always about greatness. It's about consistency. Consistent <br/> hard  work gains success. Greatness will come.</p>
+                    <a href="" className="btn">Explore Now <FaArrowRightLong /></a>
+                </div>
+                <div className="col-2">
+                    <img src="src\assets\RedStore_Img\images\image1.png" alt="image" />
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home
