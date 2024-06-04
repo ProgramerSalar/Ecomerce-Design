afd1671072ee8d4e6dd56520fd45dbb1aed29bb4
manish <manishkumar60708090@gmail.com>
2024-06-04 16:45:14 +0530

    init commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+        <div className="nav-container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
