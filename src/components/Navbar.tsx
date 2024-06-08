2394806cda134095e270ab196fe1f851536178e6
manish <manishkumar60708090@gmail.com>
2024-06-08 15:47:13 +0530

    how to use toggle item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,23 @@
 
 
 const Navbar = () => {
+
+    const menutoggle = () => {
+        
+        var MenuItems: = document.getElementById("MenuItems");
+        MenuItems.style.maxHeight == "0px"
+
+        if(MenuItems.style.maxHeight == "0px"){
+            MenuItems.style.maxHeight = "200px"
+        }
+        else{
+            MenuItems.style.maxHeight = "0px"
+            
+        }
+    }
+
+  
+
     return (
         <div className="container">
             <div className="navbar">
@@ -8,7 +25,7 @@ const Navbar = () => {
                     <img src="src\assets\RedStore_Img\images\logo.png" alt="logo" width="125px" />
                 </div>
                 <nav>
-                    <ul>  {/*  underlist tag */}
+                    <ul id="MenuItems">  {/*  underlist tag */}
                         <li><a href="">Home</a></li>  {/*  list tag */}
                         <li><a href="">Product</a></li>
                         <li><a href="">About</a></li>
@@ -16,7 +33,8 @@ const Navbar = () => {
                         <li><a href="">Account</a></li>
                     </ul>
                 </nav>
-                <img src="src\assets\RedStore_Img\images\cart.png" alt="cart-image" width="30px" height="30px"/>
+                <img src="src\assets\RedStore_Img\images\cart.png" alt="cart-image" width="30px" height="30px" />
+                <img src="src\assets\RedStore_Img\images\menu.png" alt="menu-img" className="menu-icon" onClick={() => menutoggle()} />
             </div>
         </div>
 
